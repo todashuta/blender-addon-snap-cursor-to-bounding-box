54b1d0579daf9064983a6dbea4e00479a2a5ea5a
todashuta <toda.shuta@gmail.com>
2019-08-04 17:26:33 +0900

    WIP

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Snap Cursor to Bounding Box",
     "author": "Toda Shuta",
-    "version": (1, 2, 0),
-    "blender": (2, 79, 0),
+    "version": (1, 2, 1),
+    "blender": (2, 80, 0),
     "location": "View3D > Shift-S (Snap Menu)",
     "description": "Snap Cursor to Bounding Box (Top, Center, Bottom)",
     "warning": "",
@@ -23,15 +23,11 @@ import bpy
 
 
 def register():
-    bpy.utils.register_module(__name__)
-    bpy.types.VIEW3D_MT_snap.append(snap_cursor_to_bounding_box.snap_menu_func)
-    bpy.types.VIEW3D_MT_object_specials.append(snap_cursor_to_bounding_box.special_menu_func)
+    snap_cursor_to_bounding_box.register()
 
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
-    bpy.types.VIEW3D_MT_snap.remove(snap_cursor_to_bounding_box.snap_menu_func)
-    bpy.types.VIEW3D_MT_object_specials.remove(snap_cursor_to_bounding_box.special_menu_func)
+    snap_cursor_to_bounding_box.unregister()
 
 
 if __name__ == "__main__":
